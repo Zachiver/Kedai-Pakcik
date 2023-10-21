fbbdded4c32a15dd581312752da0991582e83c6e
Zachiver <ggwpnjhniceshot@gmail.com>
2023-10-21 09:49:21 +0700

    menanbahkan fitur Search Form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Toggle Class Active
+//Toggle Class Active Untuk Hamburger Menu
 const navbarNav = document.querySelector('.navbar-nav')
 
 // Ketika hamburger menu di klik
@@ -6,11 +6,26 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 }
 
-// Clear diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu')
+// Toggle Class Active Untuk Search Form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active')
+    searchBox.focus()
+    e.preventDefault()
+}
+
+
+// Klik diluar elemen
+const hm = document.querySelector('#hamburger-menu')
+const sb = document.querySelector('#search-button')
 
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 });
